0d297275d1eb15549132ca1f4a8f985762d8e3f6
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2023-06-10 19:35:40 +0530

    Add next.js frontend skeleton

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers"
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.12",
 };
 
 export default config;
