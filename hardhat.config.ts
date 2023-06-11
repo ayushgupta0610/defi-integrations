2caea5125749c4bc9a8e08b97274090c32caf2b7
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2023-06-11 09:32:14 +0530

    Change frontend structure

--- hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers"
-import "@nomicfoundation/hardhat-toolbox";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.12",
-};
-
-export default config;
